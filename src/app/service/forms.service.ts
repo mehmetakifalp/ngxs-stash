24c43d068abd7e98acc15ed7ae023962e740564b
mehmetakifalp <mehmetakifalp@gmail.com>
2018-11-28 14:22:10 +0300

    form service

---
+++ src/app/service/forms.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from "@angular/core";
+import { AppModule } from "../app.module";
+@Injectable({
+  providedIn: AppModule,
+})
+export class FormsService{
+  constructor(){
+
+  }
+}
