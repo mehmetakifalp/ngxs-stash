df5b54fbcc69e0709bade51d955c95fda1441dd7
mehmetakifalp <mehmetakifalp@gmail.com>
2018-11-28 14:24:11 +0300

    submit forms

--- src/app/service/forms.service.ts
+++ src/app/service/forms.service.ts
@@ -1,10 +1,20 @@
 import { Injectable } from "@angular/core";
 import { AppModule } from "../app.module";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "src/environments/environment";
+
 @Injectable({
   providedIn: AppModule,
 })
 export class FormsService{
-  constructor(){
 
+  apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient){
+
+  }
+
+  submit(){
+    this.http.post(this.apiUrl + 'endpoint', {});
   }
 }
