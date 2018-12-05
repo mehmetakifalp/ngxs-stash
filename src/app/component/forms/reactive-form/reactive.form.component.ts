836070d52a1fb770aa43dfcb2e9f79b9f94214d2
mehmetakifalp <mehmetakifalp@gmail.com>
2018-12-05 09:22:01 +0300

    reactive form add new row

--- src/app/component/forms/reactive-form/reactive.form.component.ts
+++ src/app/component/forms/reactive-form/reactive.form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormGroup } from "@angular/forms";
+import { FormBuilder, FormGroup, FormArray } from "@angular/forms";
 
 @Component({
   selector: 'app-reactive-form',
@@ -28,6 +28,11 @@ export class ReactiveFormComponent implements OnInit{
 
 
   newRow(){
+    const control = <FormArray>this.invoiceForm.controls['items'];
+    control.push(this.initRows())
+  }
+
+  deleteRow(){
 
   }
 
