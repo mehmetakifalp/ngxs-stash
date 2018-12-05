cc8d8edcbea8db7e1b18ce9f77e09e149af6634c
mehmetakifalp <mehmetakifalp@gmail.com>
2018-12-05 09:20:01 +0300

    reactive form with ngxs

--- src/app/component/forms/reactive-form/reactive.form.component.ts
+++ src/app/component/forms/reactive-form/reactive.form.component.ts
@@ -1,10 +1,34 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
 
 @Component({
   selector: 'app-reactive-form',
   templateUrl: './reactive.form.component.html',
   styleUrls: ['./reactive.form.component.css']
 })
-export class ReactiveFormComponent {
+export class ReactiveFormComponent implements OnInit{
+
+  invoiceForm: FormGroup;
+
+  constructor( private fb: FormBuilder){
+
+  }
+
+  ngOnInit(){
+    this.invoiceForm = this.fb.group({
+      items: this.fb.array([this.initRows()])
+    })
+  }
+
+  initRows(){
+    return this.fb.group({
+      name: ['']
+    })
+  }
+
+
+  newRow(){
+
+  }
 
 }
